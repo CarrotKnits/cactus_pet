94deb7656ec4872b74e98319c24e70ea0850d17c
CarrotKnits <carrotsandcarrots@gmail.com>
2026-04-27 16:17:21 -0500

    add call functions for menu and cursor controls

--- main.lua
+++ main.lua
@@ -3,11 +3,14 @@
 function _init()
     music(3)
     ipet()
-
+    i_main_menu()
+    i_cursor_control()
 end
 
 function _update()
     upet()
+    u_main_menu()
+    u_cursor_control()
 end
 
 function _draw()
@@ -15,4 +18,6 @@ function _draw()
     map()
     spr(11,24,32)
     dpet()
+    d_main_menu()
+    d_cursor_control()
 end
