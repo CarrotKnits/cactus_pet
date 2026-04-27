9e5f1761d774a602c651112b78c39a830f7ff3ea
CarrotKnits <carrotsandcarrots@gmail.com>
2026-04-27 16:08:31 -0500

    add functions into main.lua so map and pet draw to screen, add music playback in _init()

--- main.lua
+++ main.lua
@@ -1,13 +1,18 @@
 --main program
 
 function _init()
+    music(3)
+    ipet()
 
 end
 
 function _update()
-
+    upet()
 end
 
 function _draw()
-
+    cls()
+    map()
+    spr(11,24,32)
+    dpet()
 end
