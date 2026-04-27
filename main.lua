9b6283ce3df0910636268ac70adda6d13d6a0088
CarrotKnits <carrotsandcarrots@gmail.com>
2026-04-27 16:26:20 -0500

    add call functions for hp and status bars

--- main.lua
+++ main.lua
@@ -3,12 +3,14 @@
 function _init()
     music(3)
     ipet()
+    i_hp_sts()
     i_main_menu()
     i_cursor_control()
 end
 
 function _update()
     upet()
+    u_hp_sts()
     u_main_menu()
     u_cursor_control()
 end
@@ -17,6 +19,7 @@ function _draw()
     cls()
     map()
     spr(11,24,32)
+    d_hp_sts()
     dpet()
     d_main_menu()
     d_cursor_control()
