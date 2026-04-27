ce5c3366a77e001063b0ad0e88d4630680b0c71e
CarrotKnits <carrotsandcarrots@gmail.com>
2026-04-27 16:14:59 -0500

    create cursor.lua, add cursor draw, cursor controls, cursor sfx feedback

---
+++ cursor.lua
@@ -0,0 +1,64 @@
+--cursor controls
+
+--cursor control
+function i_cursor_control()
+	cursr={
+		tlx=7,
+		tly=38,
+		blx=16,
+		bly=47,
+		clr=7,
+		state="wash"
+	}
+end
+
+function u_cursor_control()
+
+--cursor response to button press ➡️
+	if btnp(➡️) then
+		sfx(0)
+		if cursr.state=="wash" then
+			cursr.state="water"
+		elseif cursr.state=="water" then
+			cursr.state="talk"
+		elseif cursr.state=="talk" then
+			cursr.state="wash"
+		end
+	end
+--cursor response to button press ⬅️
+	if btnp(⬅️) then
+		sfx(0)
+		if cursr.state=="wash" then
+			cursr.state="talk"
+		elseif cursr.state=="water" then
+			cursr.state="wash"
+		elseif cursr.state=="talk" then
+			cursr.state="water"
+		end
+	end
+	
+--change cursor state
+	if cursr.state=="wash" then
+		cursr.tlx=7
+		cursr.blx=16
+		menu.state="wash"
+	elseif cursr.state=="water" then
+		cursr.tlx=23
+		cursr.blx=32
+		menu.state="water"
+	elseif cursr.state=="talk" then
+		cursr.tlx=39
+		cursr.blx=48
+		menu.state="talk"
+	end
+--select option
+	if btnp(❎) then
+		sfx(1)
+	end
+
+end
+
+--draw 
+function d_cursor_control()
+	rect(cursr.tlx,cursr.tly,cursr.blx,cursr.bly,cursr.clr)
+end
