5a777b77ec9087d89399ba86840c4d0f1d37dec5
CarrotKnits <carrotsandcarrots@gmail.com>
2026-04-27 16:12:50 -0500

    create menu.lua, add menu state machine to change sprites when hover over options, add menu UI

---
+++ menu.lua
@@ -0,0 +1,63 @@
+--main menu
+
+--initialize main menu
+function i_main_menu()
+--setup menu
+	wash={
+		sp=17,
+		x=8,
+		y=40
+	}
+	
+	water={
+		sp=18,
+		x=24,
+		y=40
+	}
+	
+	talk={
+		sp=19,
+		x=40,
+		y=40
+	}
+	
+	menu={
+		state="wash"
+	}
+	
+end
+
+--update main menu
+function u_main_menu()
+	if menu.state=="wash" then
+		wash.sp=1
+		wash.y=39
+	else
+		wash.sp=17
+		wash.y=40
+	end
+	
+	if menu.state=="water" then
+		water.sp=2
+		water.y=39
+	else
+		water.sp=18
+		water.y=40
+	end
+	
+	if menu.state=="talk" then
+		talk.sp=3
+		talk.y=39
+	else
+		talk.sp=19
+		talk.y=40
+	end
+
+end
+
+--draw menu
+function d_main_menu()
+	spr(wash.sp,wash.x,wash.y)
+	spr(water.sp,water.x,water.y)
+	spr(talk.sp,talk.x,talk.y)
+end
