7c9ff96378f8bdeabfbb5cc3b244dfa8c524ae52
CarrotKnits <carrotsandcarrots@gmail.com>
2026-04-27 16:02:21 -0500

    create pet.lua, create pet stats and pet idle animation

---
+++ pet.lua
@@ -0,0 +1,36 @@
+--pet
+
+function ipet()
+	petx=24
+	pety=24
+	p_spr=5
+	pi={
+		sp=p_spr,
+		x=petx,
+		y=pety,
+		t=0
+	} --pet idle
+	
+	pet_stats = {
+	hp=38,
+	hap=38,
+	cln=38
+	}
+	
+end
+
+function upet()
+	pi.t+=0.05
+	pet_stats.hp-=0.005
+	pet_stats.hap-=0.005
+	pet_stats.cln-=0.005
+end
+
+function dpet()
+	--pet idle animation
+	sspr(48,5,5,3,pi.x,pi.y+5) --base
+	local bob = sin(pi.t) * 1
+	sspr(40,0,8,7,pi.x,pi.y+bob) --top
+	
+	--
+end    
