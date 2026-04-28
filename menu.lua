5f25b6cf3c2a4a9133719ce95093eeb4b1b3da8c
CarrotKnits <carrotsandcarrots@gmail.com>
2026-04-28 14:46:34 -0500

    make code more readable by adding a space before and after operators

--- menu.lua
+++ menu.lua
@@ -3,61 +3,61 @@
 --initialize main menu
 function i_main_menu()
 --setup menu
-	wash={
-		sp=17,
-		x=8,
-		y=40
+	wash = {
+		sp = 17,
+		x = 8,
+		y = 40
 	}
 	
-	water={
-		sp=18,
-		x=24,
-		y=40
+	water = {
+		sp = 18,
+		x = 24,
+		y = 40
 	}
 	
 	talk={
-		sp=19,
-		x=40,
-		y=40
+		sp = 19,
+		x = 40,
+		y = 40
 	}
 	
-	menu={
-		state="wash"
+	menu = {
+		state = "wash"
 	}
 	
 end
 
 --update main menu
 function u_main_menu()
-	if menu.state=="wash" then
-		wash.sp=3
-		wash.y=39
+	if menu.state == "wash" then
+		wash.sp = 3
+		wash.y = 39
 	else
-		wash.sp=19
-		wash.y=40
+		wash.sp = 19
+		wash.y = 40
 	end
 	
-	if menu.state=="water" then
-		water.sp=1
-		water.y=39
+	if menu.state == "water" then
+		water.sp = 1
+		water.y = 39
 	else
-		water.sp=17
-		water.y=40
+		water.sp = 17
+		water.y = 40
 	end
 	
-	if menu.state=="talk" then
-		talk.sp=2
-		talk.y=39
+	if menu.state == "talk" then
+		talk.sp = 2
+		talk.y = 39
 	else
-		talk.sp=18
-		talk.y=40
+		talk.sp = 18
+		talk.y = 40
 	end
 
 end
 
 --draw menu
 function d_main_menu()
-	spr(wash.sp,wash.x,wash.y)
-	spr(water.sp,water.x,water.y)
-	spr(talk.sp,talk.x,talk.y)
+	spr(wash.sp, wash.x, wash.y)
+	spr(water.sp, water.x, water.y)
+	spr(talk.sp, talk.x, talk.y)
 end
--- stats.lua
+++ stats.lua
@@ -42,7 +42,7 @@ function i_hp_sts()
 end
 
 function u_hp_sts()
-	--hp states
+--hp states
 	if pet_stats.cln>=mx_br_sz*.9 then
 		hp.state="max"
 	elseif pet_stats.hp<=mx_br_sz*.75 and pet_stats.hp>=mx_br_sz*.5 then
@@ -58,13 +58,13 @@ function u_hp_sts()
 	elseif pet_stats.hp==0 then
 		hp.state="dead"
 	end
-	--happiness states
+--happiness states
 	if pet_stats.hap<=mx_br_sz/2 then
 		cln.state="sad"
 	else
 		cln.state="happy"
 	end
-	--cleanliness states
+--cleanliness states
 	if pet_stats.cln<=mx_br_sz/2 then
 		cln.state="dirty"
 	else
@@ -74,23 +74,22 @@ function u_hp_sts()
 end
 
 function d_hp_sts()
---	rectfill(3,3,23,21,1)
 --hp
-	spr(hp.sp,hp.spx,hp.spy)
-	rectfill(hp.tlx,hp.tly,hp.blx,hp.bly,1)
-	rectfill(hp.tlx,hp.tly,hp.tlx+1+pet_stats.hp,hp.bly,hp.clr)
-	rect(hp.tlx-1,hp.tly-1,hp.blx+1,hp.bly+1,1)
+	spr(hp.sp, hp.spx, hp.spy)
+	rectfill(hp.tlx, hp.tly, hp.blx, hp.bly, 1)
+	rectfill(hp.tlx, hp.tly, hp.tlx + 1 + pet_stats.hp, hp.bly, hp.clr)
+	rect(hp.tlx - 1, hp.tly - 1, hp.blx + 1, hp.bly + 1, 1)
 
 --happiness: bug:happiness bar is cln bar, why??
-	spr(hap.sp,hap.spx,hap.spy)
-	rectfill(hap.tlx,hap.tly,hap.blx,hap.bly,1)
-	rectfill(hap.tlx,hap.tly,hap.tlx+1+pet_stats.hap,hap.bly,hap.clr)
-	rect(hap.tlx-1,hap.tly-1,hap.blx+1,hap.bly+1,1)
+	spr(hap.sp, hap.spx, hap.spy)
+	rectfill(hap.tlx, hap.tly, hap.blx, hap.bly, 1)
+	rectfill(hap.tlx, hap.tly, hap.tlx + 1 + pet_stats.hap, hap.bly, hap.clr)
+	rect(hap.tlx - 1, hap.tly - 1, hap.blx + 1, hap.bly + 1, 1)
 
 --cleanliness: bug:cln bar is happiness bar, why?? 
-	spr(cln.sp,cln.spx,cln.spy)
-	rectfill(cln.tlx,cln.tly,cln.blx,cln.bly,1)
-	rectfill(cln.tlx,cln.tly,cln.tlx+1+pet_stats.cln,cln.bly,cln.clr)
-	rect(cln.tlx-1,cln.tly-1,cln.blx+1,cln.bly+1,1)
+	spr(cln.sp, cln.spx, cln.spy)
+	rectfill(cln.tlx, cln.tly, cln.blx, cln.bly, 1)
+	rectfill(cln.tlx, cln.tly, cln.tlx + 1 + pet_stats.cln, cln.bly, cln.clr)
+	rect(cln.tlx - 1, cln.tly - 1, cln.blx + 1, cln.bly + 1, 1)
 
 end
