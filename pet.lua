49cd539785844cd7b47e19a39fd5028773b684bb
CarrotKnits <carrotsandcarrots@gmail.com>
2026-04-28 14:47:48 -0500

    CHANGE stat decay speed to be higher for testing, MAKE code more readable by adding a space before and after operators

--- pet.lua
+++ pet.lua
@@ -1,32 +1,34 @@
 --pet
 
 function ipet()
-	petx=24
-	pety=24
-	p_spr=5
-	pi={
-		sp=p_spr,
-		x=petx,
-		y=pety,
-		t=0
+	petx = 24
+	pety = 24
+	p_spr = 5
+	pi = {
+		sp = p_spr,
+		x = petx,
+		y = pety,
+		t = 0
 	} --pet idle
 	
 	pet_stats = {
-	maxhp=38,
-	hp=38,
-	maxhap=38,
-	hap=38,
-	maxcln=38,
-	cln=38
+	maxhp = 38,
+	hp = 38,
+	maxhap = 38,
+	hap = 38,
+	maxcln = 38,
+	cln = 38
 	}
 	
 end
 
 function upet()
-	pi.t+=0.05
-	pet_stats.hp-=0.005
-	pet_stats.hap-=0.005
-	pet_stats.cln-=0.005
+	--pet animation speed
+	pi.t += 0.05
+	--stat decay speeds, defaults 0.005
+	pet_stats.hp -= 0.1
+	pet_stats.hap -= 0.1
+	pet_stats.cln -= 0.1
 end
 
 function dpet()
