f17de1d236c179dab644ffffe38020872ba9009f
CarrotKnits <carrotsandcarrots@gmail.com>
2026-05-04 20:55:11 -0500

    CREATE animations table

---
+++ pet_animation.lua
@@ -0,0 +1,45 @@
+--pet animations
+
+function i_ani()
+    animation = {
+        --max
+        mch = {4,64},
+        mcs = {20,80},
+        mdh = {36,96},
+        mds = {52,112},
+        --healthy
+        hch = {5,65},
+        hcs = {21,81},
+        hdh = {37,97},
+        hds = {53,113},
+        --normal
+        nch = {6,66},
+        ncs = {22,88},
+        ndh = {38,98},
+        nds = {54,114},
+        --weakening
+        wnch = {7,67},
+        wncs = {23,83},
+        wndh = {39,99},
+        wnds = {55,115},
+        --weak
+        wkch = {8,68},
+        wkcs = {24,84},
+        wkdh = {40,100},
+        wkds = {56,116},
+        --dying
+        dych = {9,69},
+        dycs = {25,85},
+        dydh = {41,101},
+        dyds = {57,117},
+        --dead
+        ddch = {10,70},
+        ddcs = {26,86},
+        dddh = {42,102},
+        ddds = {58,118},
+    }
+end
+
+function u_ani()
+
+end
