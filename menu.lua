dff1e760ee7ab8e6470f3222735787ed77abbe2c
CarrotKnits <carrotsandcarrots@gmail.com>
2026-05-06 13:52:00 -0500

    CHANGE implemention of cooldown timers for actions

--- menu.lua
+++ menu.lua
@@ -6,60 +6,113 @@ function i_main_menu()
 	wash = {
 		sp = 17,
 		x = 8,
-		y = 40,
-		wash_cd = 150,
-		state = "ready"
+		y = 40
 	}
 	
 	water = {
 		sp = 18,
 		x = 24,
-		y = 40,
-		water_cd = 150,
-		state = "ready"
+		y = 40
 	}
 	
 	talk={
 		sp = 19,
 		x = 40,
-		y = 40,
-		talk_cd = 150,
-		state = "ready"
+		y = 40
 	}
 	
 	menu = {
 		state = "wash"
 	}
 	
+	--cooldown timers
+	--wash cd
+	wash_cd = 0
+	wash_cd_max = 150
+
+	--water cd
+	water_cd = 0
+	water_cd_max = 150
+
+	--talk_cd
+	talk_cd = 0
+	talk_cd_max = 150
+
+
 end
 
 --update main menu
 function u_main_menu()
-	if menu.state == "wash" then
-		wash.sp = 3
-		wash.y = 39
-	else
-		wash.sp = 19
-		wash.y = 40
+
+	if wash_cd > 0 then
+		wash_cd -= 1
 	end
-	
-	if menu.state == "water" then
-		water.sp = 1
-		water.y = 39
-	else
-		water.sp = 17
-		water.y = 40
+
+	if water_cd > 0 then
+		water_cd -= 1
 	end
-	
-	if menu.state == "talk" then
-		talk.sp = 2
-		talk.y = 39
+
+	if talk_cd > 0 then
+		talk_cd -= 1
+	end
+
+	if wash_cd ~= 0 then
+		if menu.state == "wash" then
+			wash.sp = 59
+			wash.y = 39
+		else
+			wash.sp = 59
+			wash.y = 40
+		end
 	else
-		talk.sp = 18
-		talk.y = 40
+		if menu.state == "wash" then
+			wash.sp = 3
+			wash.y = 39
+		else
+			wash.sp = 19
+			wash.y = 40
+		end
+	end
+
+	if talk_cd ~= 0 then
+		if menu.state == "talk" then
+			talk.sp = 43
+			talk.y = 39
+		else
+			talk.sp = 43
+			talk.y = 40
+		end
+	else
+		if menu.state == "talk" then
+			talk.sp = 2
+			talk.y = 39
+		else
+			talk.sp = 18
+			talk.y = 40
+		end
+	end
+
+	if water_cd ~= 0 then
+		if menu.state == "water" then
+			water.sp = 27
+			water.y = 39
+		else
+			water.sp = 27
+			water.y = 40
+		end
+	else
+		if menu.state == "water" then
+			water.sp = 1
+			water.y = 39
+		else
+			water.sp = 17
+			water.y = 40
+		end
 	end
 
 end
+
+
 
 --draw menu
 function d_main_menu()
